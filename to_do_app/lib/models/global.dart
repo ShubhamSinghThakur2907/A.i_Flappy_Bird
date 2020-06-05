478b4736ac18e5488cddb186ffcb06bdcea1fc5f
Kartikey Shaurya <kartikeyshaurya@yahoo.com>
2020-06-05 20:00:11 +0530

    new colors added

---
+++ to_do_app/lib/models/global.dart
@@ -0,0 +1,6 @@
+import "package:flutter/material.dart" ; 
+
+Color darkGreyColor = new Color(0xFF212128);
+Color myColor = new Color(0xFFB9F6CA);
+
+TextStyle intrayTitleStyle = new TextStyle(fontFamily: "Avenir", fontWeight: FontWeight.bold,fontSize: 45 ) ;
