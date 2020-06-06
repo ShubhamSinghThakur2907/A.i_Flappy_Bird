f45e9c3c3d74b9c637ca33c62360b3d104268da6
Kartikey Shaurya <kartikeyshaurya@yahoo.com>
2020-06-07 01:20:20 +0530

    messed badly means b=irreversal

--- to_do_app/lib/models/global.dart
+++
@@ -1,6 +0,0 @@
-import "package:flutter/material.dart" ; 
-
-Color darkGreyColor = new Color(0xFF212128);
-Color myColor = new Color(0xFFB9F6CA);
-
-TextStyle intrayTitleStyle = new TextStyle(fontFamily: "Avenir", fontWeight: FontWeight.bold,fontSize: 45 ) ;
