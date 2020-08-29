652317662d7b6bdaa5e165e6e4db43899594ba42
Kartikey Shaurya <kartikeyshaurya@yahoo.com>
2020-08-29 20:28:55 +0530

    intial flappy bird class

---
+++ flappy_bird.py
@@ -0,0 +1,44 @@
+import pygame 
+import neat 
+import time 
+import os 
+import random
+
+WIN_WIDTH = 600 
+WIN_HEIGHT = 800   #CAPITAL HENCE CONSTANT 
+
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("all_assets", "icon", "bluebird-upflap.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("all_assets", "icon", "bluebird-midflap.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("all_assets", "icon", "bluebird-downflap.png")))]
+
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("all_assets", "icon", "pipe-red.png")))
+
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("all_assets", "icon", "base.png")))
+
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("all_assets", "icon", "background-night.png")))
+
+
+class bird:
+    IMGS = BIRD_IMGS
+    MAX_ROTATION = 25 
+    ROT_VEL = 20
+    ANIMATION_TIME = 5 
+
+    def __init__(self, x, y):
+        self.x = x 
+        self.y = y 
+        self.tilt = 0 
+        self.vol = 0 
+        self.height = self.y 
+        self.img_count = 0 
+        self.img = self.IMGS[0]
+    
+    def jump(self):
+        self.vel = -10.5
+        self.tick_count = 0 
+        self.height = self.y 
+
+    def move(self):
+        pass 
+
+while True:
+    bird.move()
+
