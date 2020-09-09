bb787fce4a61a09eda5500670a315f91cb678139
Kartikey Shaurya <kartikeyshaurya@yahoo.com>
2020-09-09 19:05:17 +0530

    completed move function

--- flappy_bird.py
+++ flappy_bird.py
@@ -17,6 +17,12 @@ BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("all_assets", "
 
 
 class bird:
+    """
+    IMGS : images of the flappy bird 
+    MAX_ROTATION : maximum rotation that flappy bird can take (tilt)
+    ROT_VAL : the velocity of the rotation per frame 
+    ANIMATION_TIME : how much the bird can fly in a frame  
+    """
     IMGS = BIRD_IMGS
     MAX_ROTATION = 25 
     ROT_VEL = 20
@@ -37,7 +43,27 @@ class bird:
         self.height = self.y 
 
     def move(self):
-        pass 
+        self.tick_count += 1 
+
+        d = self.vel*self.tick_count + 1.5*self.tick_count **2 
+
+        if d>= 16:
+            d = 16 
+
+        if  d< 0 :
+            d -= 2
+
+        self.y = self.y + 4 
+
+        if d< 0 or self.y = self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+            else:
+                if self.tilt > -90 :
+                    self.tilt -=  self.ROT_VEL
+
+        
+        
 
 while True:
     bird.move()
