81aab2c30f6b494d9f72ade071d23b2e37655007
Kartikey Shaurya <kartikeyshaurya@yahoo.com>
2020-09-13 12:06:50 +0530

    decided to keep the flappy bird functions in the seperate file

--- class_flappy_bird.py
+++ class_flappy_bird.py
@@ -178,28 +178,3 @@ def blitRotateCenter(surf, image, topleft, angle):
 
     surf.blit(rotated_image, new_rect.topleft)
 
-def main():
-    bird = Bird(230, 350)
-    base = Base(730)
-    pipes = [Pipe(700)]
-    win = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
-    clock = pygame.time.Clock()
-
-    run = True 
-    while run:
-        clock.tick(120)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-
-
-        # bird.move()
-        for pipe in pipes:
-            pipe.move()
-        base.move()
-        draw_window(win, bird, pipes, base)
-
-    pygame.quit()
-    quit()
-
-main()
